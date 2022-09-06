6e8651a92709215ccb3d8f3e26283f65d5c91b16
apm29 <yjw999wow@163.com>
2022-09-06 17:31:32 +0800

    fix: control - arrow-key move

--- src/digital-twin/3d/helpers/core.js
+++ src/digital-twin/3d/helpers/core.js
@@ -201,7 +201,7 @@ export function createSky(scene) {
 
 export function createControl(camera, element) {
   const controls = new OrbitControls(camera, element);
-
+  controls.listenToKeyEvents(window); // optional
   controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
   controls.dampingFactor = 0.05;
   controls.screenSpacePanning = false;

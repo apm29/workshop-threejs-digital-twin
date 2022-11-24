d1ad31250d24d7dcad165855fa7996bd61d210da
apm29 <yjw999wow@163.com>
2022-11-24 08:43:24 +0800

    feat: 全屏按钮

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <NameSpace h="screen" w="screen" namespace="app" class="atcc-workshop">
+  <NameSpace
+    h="screen"
+    w="screen"
+    namespace="app"
+    class="atcc-workshop"
+    ref="digitalTwinRef"
+  >
     <Renderer h="screen" w="screen">
       <Camera>
         <Scene>
@@ -16,6 +22,11 @@
       </Camera>
     </Renderer>
     <Frame></Frame>
+    <el-tooltip content="全屏">
+      <button @click="toggleFullscreen" v-if="!isFullscreen" class="fixed right-4 top-4">
+        <i class="el-icon-full-screen text-lg font-bold text-blue-500" />
+      </button>
+    </el-tooltip>
   </NameSpace>
 </template>
 
@@ -25,12 +36,13 @@ import Scene from "~/digital-twin/components/Scene.vue";
 import Camera from "~/digital-twin/components/Camera.vue";
 import Sky from "~/digital-twin/components/Sky.vue";
 import Light from "~/digital-twin/components/Light.vue";
-import Ground from "~/digital-twin/components/Ground.vue";
-import CementGround from "~/digital-twin/components/CementGround.vue";
 import Loading from "~/digital-twin/components/Loading.vue";
 import Models from "~/digital-twin/combine/Models.vue";
 import Frame from "~/digital-twin/combine/Frame.vue";
 import Controls from "~/digital-twin/components/Controls.vue";
 import PostProcess from "~/digital-twin/components/PostProcess.vue";
 import NameSpace from "~/digital-twin/components/NameSpace.vue";
+
+const digitalTwinRef = ref();
+const { isFullscreen, toggle: toggleFullscreen } = useFullscreen(digitalTwinRef);
 </script>
